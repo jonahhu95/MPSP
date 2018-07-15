9103360f6d522d0d440f7013a4bb39abbdfa2447
Daniel Tang Yi Hao <danieltang3434@gmail.com>
2018-07-15 08:44:58 +0800

    .

--- app/src/main/java/monster/artificial/bannerlah/MapActivity.java
+++ app/src/main/java/monster/artificial/bannerlah/MapActivity.java
@@ -1,7 +1,7 @@
 package monster.artificial.bannerlah;
 
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -9,26 +9,34 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.maps.android.clustering.ClusterManager;
-
 import org.json.JSONException;
-
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 import monster.artificial.bannerlah.model.MapItem;
 
-public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
+
     // Declare a variable for the cluster manager.
     private ClusterManager<MapItem> mClusterManager;
-
     private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+//        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+//                .findFragmentById(R.id.map);
+//        mapFragment.getMapAsync(this);
         setUpMap();
+    }
+
+    private void setUpMap() {
+        ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMapAsync(this);
     }
 
     @Override
@@ -46,63 +54,36 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         startDemo();
     }
 
-//    private void setUpClusterer() {
-//        // Position the map.
-//        getMapAsync(this).moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(51.503186, -0.126446), 10));
-//
-//        // Initialize the manager with the context and the map.
-//        // (Activity extends context, so we can pass 'this' in the constructor.)
-//        mClusterManager = new ClusterManager<MapItem>(this, getMap());
-//
-//        // Point the map's listeners at the listeners implemented by the cluster
-//        // manager.
-//        getMap().setOnCameraIdleListener(mClusterManager);
-//        getMap().setOnMarkerClickListener(mClusterManager);
-//
-//        // Add cluster items (markers) to the cluster manager.
-//        addItems();
-//    }
-
-//    private void addItems() {
-//
-//        // Set some lat/lng coordinates to start with.
-//        double lat = 51.5145160;
-//        double lng = -0.1270060;
-//
-//        // Add ten cluster items in close proximity, for purposes of this example.
-//        for (int i = 0; i < 10; i++) {
-//            double offset = i / 60d;
-//            lat = lat + offset;
-//            lng = lng + offset;
-//            MapItem offsetItem = new MapItem(lat, lng);
-//            mClusterManager.addItem(offsetItem);
-//        }
-//    }
-
     protected void startDemo() {
-        getMap().moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(51.503186, -0.126446), 10));
+        double v1 = 5.369279;
+        double v2 = 100.431960;
 
-        mClusterManager = new ClusterManager<MapItem>(this, getMap());
-        getMap().setOnCameraIdleListener(mClusterManager);
-
-        try {
-            readItems();
-        } catch (JSONException e) {
-            Toast.makeText(this, "Problem reading list of markers.", Toast.LENGTH_LONG).show();
+        // at Jalan Perda Utama
+        for (int i = 0 ; i < 15; i++)
+        {
+            LatLng Location = new LatLng(v1, v2);
+            mMap.addMarker(new MarkerOptions().position(Location).title("Banners"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Location, 15));
+            v1 += 0.000075;
+            v2 += 0.000250;
         }
-    }
 
-    private void readItems() throws JSONException {
-        InputStream inputStream = getResources().openRawResource(R.raw.radar_search);
-        List<MapItem> items = new MapItemReader().read(inputStream);
-        mClusterManager.addItems(items);
-    }
+        v1 = 5.369074;
+        v2 = 100.431397;
 
-    private void setUpMap() {
-        ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMapAsync(this);
-    }
+        // at Jalan Perda Utama
+        for (int i = 0 ; i < 15; i++)
+        {
+            LatLng Location = new LatLng(v1, v2);
+            mMap.addMarker(new MarkerOptions().position(Location).title("Banners"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Location, 15));
+            v1 -= 0.000075;
+            v2 -= 0.000250;
+        }
 
-    protected GoogleMap getMap() {
-        return mMap;
+        // Random points
+
+
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(15), 2000, null);
     }
 }
--- app/src/main/java/monster/artificial/bannerlah/model/MapItem.java
+++ app/src/main/java/monster/artificial/bannerlah/model/MapItem.java
@@ -1,4 +1,34 @@
 package monster.artificial.bannerlah.model;
 
-public class MapItem {
+import com.google.android.gms.maps.model.LatLng;
+import com.google.maps.android.clustering.ClusterItem;
+
+public class MapItem implements ClusterItem {
+    private final LatLng mPosition;
+    private final String mTitle;
+    private final String mSnippet;
+
+    public MapItem(double lat, double lng) {
+        mPosition = new LatLng(lat, lng);
+        mTitle = null;
+        mSnippet = null;
+    }
+
+    public MapItem(double lat, double lng, String title, String snippet) {
+        mPosition = new LatLng(lat, lng);
+        mTitle = title;
+        mSnippet = snippet;
+    }
+
+    public LatLng getPosition() {
+        return mPosition;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public String getSnippet() {
+        return mSnippet;
+    }
 }
